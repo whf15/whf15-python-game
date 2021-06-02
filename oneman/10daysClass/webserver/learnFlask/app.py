b3b0179a58585e051d1d56da457a60e5f1d4d884
whf <1211368233@qq.com>
2021-06-02 16:52:46 +0800

    handler 404 db

--- oneman/10daysClass/webserver/learnFlask/app.py
+++ oneman/10daysClass/webserver/learnFlask/app.py
@@ -1,14 +1,58 @@
-from flask import Flask
+from flask import (Flask,request,
+                send_from_directory,render_template,
+                redirect,url_for)
+import pymysql
+import pymysql.cursors
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():
     return 'Hello World! Whf15'
 
-@app.route('/hello')
-def hello_Whf15():
-    return 'Hello Whf15!'
+@app.route('/hello/<name>')
+def hello_Whf15(name=None):
+    return 'Hello : '+ name
+
+@app.route('/getpost/<int:postid>')
+def postId(postid):
+    return 'Hello : '+ str(postid)
+
+@app.route('/template/<name>')
+def template_hello(name=None):
+    return render_template("mytest.html",user=name)
+    # 方法一.在该py文件的同级目录下,建立templates文件夹,将test.html文件放入其中.
+
+#外部链接的跳转
+@app.route('/redirect')
+def redirect_test():
+    # return redirect("/hello")
+    return redirect(url_for("hello_world"))
+
+#捕捉异常404
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"),404
+
+#connect db
+@app.route("/db/<name>")
+def get_db_connect(name):
+    connection = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="whfoxxx1999",
+        db="sakila",
+        charset="utf8",
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM sakila.actor limit 10;")
+        result = cursor.fetchall()
+        return render_template("actorlist.html",actor=result)
+
+    return render_template("404.html"),404
+
+
 
 if __name__ == '__main__':
     app.run()
