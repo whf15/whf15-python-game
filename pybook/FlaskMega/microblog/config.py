adb78b0facf9fa1461b3ce59ad52739715628f3a
whf <1211368233@qq.com>
2021-11-23 23:58:58 +0800

    db

--- pybook/FlaskMega/microblog/config.py
+++ pybook/FlaskMega/microblog/config.py
@@ -5,3 +5,12 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    # ...
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
