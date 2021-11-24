4b003ddbdc946c7a6636f512ea1de4a6cdf4d9ff
whf <1211368233@qq.com>
2021-11-25 00:19:12 +0800

    User

--- pybook/FlaskMega/microblog/app/models.py
+++ pybook/FlaskMega/microblog/app/models.py
@@ -3,4 +3,9 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    # 用于在调试时打印用户实例
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
