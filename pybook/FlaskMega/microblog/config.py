595f7161d66786ae3c92a1ea899962a9f0127617
whf <1211368233@qq.com>
2021-11-24 23:57:28 +0800

    SQLAlchemy

--- pybook/FlaskMega/microblog/config.py
+++ pybook/FlaskMega/microblog/config.py
@@ -1,16 +1,12 @@
 # CSRF_ENABLED = True
 # SECRET_KEY = 'you-will-never-guess'
-
-import os
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    # ...
+    
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
