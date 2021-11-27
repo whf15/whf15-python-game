69aee30b598f5d0465f9ae0718e4d64cb7182a9e
whf <1211368233@qq.com>
2021-11-27 23:39:33 +0800

    数据库关系

--- pybook/FlaskMega/microblog/app/models.py
+++ pybook/FlaskMega/microblog/app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -5,7 +6,19 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    # 用户与其动态之间关系的高级视图，一般relationship字段处于‘一’
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     # 用于在调试时打印用户实例
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+# 表示用户发表的动态
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post{}>'.format(self.body)
