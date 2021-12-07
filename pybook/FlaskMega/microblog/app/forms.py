4e9eeb498172c95b15e7d00524621d10e2f71f18
whf <1211368233@qq.com>
2021-12-07 23:52:37 +0800

    错误机制

--- pybook/FlaskMega/microblog/app/forms.py
+++ pybook/FlaskMega/microblog/app/forms.py
@@ -43,3 +43,6 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
