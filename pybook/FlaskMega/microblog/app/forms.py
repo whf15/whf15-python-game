04f45e5b71b2bb9a6fe0fbb11f0c6db2577925e0
whf <1211368233@qq.com>
2021-12-08 23:49:05 +0800

    修改名字不能重复

--- pybook/FlaskMega/microblog/app/forms.py
+++ pybook/FlaskMega/microblog/app/forms.py
@@ -46,3 +46,10 @@ class EditProfileForm(FlaskForm):
 
     def __init__(self, original_username, *args, **kwargs):
         super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please  use a different username!')
