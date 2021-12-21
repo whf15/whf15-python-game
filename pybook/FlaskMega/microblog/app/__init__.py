b242f4736f97650d4da675b391e68ffde8d173bf
whf <1211368233@qq.com>
2021-12-21 23:20:58 +0800

    request

--- pybook/FlaskMega/microblog/app/__init__.py
+++ pybook/FlaskMega/microblog/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask,request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -21,3 +21,8 @@ moment = Moment(app)
 babel = Babel(app)
 
 from app import routes, models, errors
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    
